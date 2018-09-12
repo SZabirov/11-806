a6df524083dca3d7b5fc44a789d47610f523bfb2
Salavat Zabirov <zab.sal42@gmail.com>
2018-09-12 09:50:33 +0300

    comments to Main.java added

--- Lesson04/src/com/company/Main.java
+++ Lesson04/src/com/company/Main.java
@@ -2,18 +2,22 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-	    char c = 'd';
+	    //получение числового кода для данного символа
+        char c = 'd';
         int intC = c;
         System.out.println(intC);
 
+        //нахождение символа с данным числовым кодом
         int x = 70;
         char charX = (char) x;
         System.out.println(charX);
 
+        //получение числового значения символа-цифры
         char digit = '5';
         int intDigit = digit - '0';
         System.out.println(intDigit);
 
+        //перевод буквы из одного регистра в другой
         char letter = 'H';
         if (letter >= 'A' && letter <= 'Z') {
             letter = (char) (letter + 32);
