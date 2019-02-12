563a39f081bfce68072395d0c28a348622bef4a4
Salavat Zabirov <zab.sal42@gmail.com>
2019-02-12 14:14:23 +0300

    Lesson21 update for comparable task

--- Lesson21/src/com/company/AudioTrack.java
+++ Lesson21/src/com/company/AudioTrack.java
@@ -10,7 +10,12 @@ public class AudioTrack implements Comparable<AudioTrack> {
     //0 - равен
     @Override
     public int compareTo(AudioTrack o) {
-        return duration - o.duration;
+        int d = duration - o.duration;
+        if (d == 0) {
+            return title.compareTo(o.title);
+        } else {
+            return d;
+        }
 //        if (duration > o.duration) {
 //            return 1;
 //        } else {
--- Lesson21/src/com/company/SortedPlayList.java
+++ Lesson21/src/com/company/SortedPlayList.java
@@ -2,8 +2,18 @@ package com.company;
 
 public class SortedPlayList {
     private AudioTrack[] arr;
+    int n = 0;
 
+    public SortedPlayList(int size) {
+        this.arr = new AudioTrack[size];
+    }
+
+    //вставляет новый трек в массив так, чтобы
+    //треки в нем остались упорядочены
     public void add(AudioTrack audioTrack) {
-
+        int c = 0;
+        while (arr[c].compareTo(audioTrack) <= 0) {
+            c++;
+        }
     }
 }
