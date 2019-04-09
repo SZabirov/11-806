3bcb29f86589082a46c6ce0290bc8e8e1a611c5c
Salavat Zabirov <zab.sal42@gmail.com>
2019-04-09 10:45:36 +0300

    Lesson36. Threads task added

--- Lesson36/src/com/company/Main.java
+++ Lesson36/src/com/company/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static int mainSum = 0;
 
     public static void main(String[] args) {
-
+        //завести массив, заполнить рандомом
+        //создать n counterThread'ов и запустить
+        //т. о. получим сумму элементов в массиве
+        //в переменной mainSum
+        //Проверить результат следующим образом:
+        //прямо в main'e посчитать обычным for'ом
+        //сумму эл-тов массива, сравнить с mainSum
 
     }
 }
